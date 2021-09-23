dba74f45485805268494344ab71186f01e3f5d80
alberto <albertozanola1996@gmail.com>
2021-09-23 11:14:38 +0200

    avanzamento

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header/>
+
+    <Main/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+import Main from './components/Main.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    Main
   }
 }
 </script>
 
 <style lang="scss">
-@import "./style/general.scss";
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
 
 </style>
